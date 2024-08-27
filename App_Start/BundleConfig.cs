12ca9e8988b27aa25313f6aadc2231d92f9fafa5
chinmayi.bhat <chinmayi.bhat@YCA-LAP-319>
2024-08-27 16:40:55 +0530

    Add project files.

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Optimization;
+
+namespace StackOverflowProject.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include("~/Scripts/jQuery-3.3.1.js", "~/Scripts/umd/popper.js", "~/Scripts/umd/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/Styles/bootstrap").Include("~/Content/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Styles/site").Include("~/Content/Style.css"));
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
